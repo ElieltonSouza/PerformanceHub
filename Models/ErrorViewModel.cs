c78b5d89d557dc79e4b8f8c8b1df2e1b71122c6d
ElieltonSouza <elielton.silva@aluno.ueg.br>
2024-11-28 15:59:31 -0300

    Inicialização do projeto PerformanceHub

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PerformanceHub.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
